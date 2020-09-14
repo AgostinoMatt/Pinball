8d78f07ed44b510e50c935f8f7cf95ecc0397d42
AgostinoMatt <AgostinoMatt@users.noreply.github.com>
2020-09-14 17:08:27 -0400

    some script

---
+++ Pinball/Assets/Pinball Creator/OnscreenButtonController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Windows.Input;
+
+public class OnscreenButtonController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RightFlipper()
+    {
+        
+    }
+
+    public void LeftFlipper()
+    {
+
+    }
+
+    public void Launch()
+    {
+
+    }
+
+    public void Shake()
+    {
+
+    }
+}
